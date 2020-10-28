d9b810119e32a333eada126d8eee9382fd8bae07
Huy Doan <doanvanhuy0907@gmail.com>
2020-10-28 23:10:39 +0700

    filter shoes

--- js/shoes.js
+++ js/shoes.js
@@ -33,7 +33,7 @@ function w3RemoveClass(element, name) {
 
 // Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
+var btns = btnContainer.getElementsByClassName("btn-filter");
 for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function() {
         var current = document.getElementsByClassName("active");
