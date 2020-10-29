d76f809e13907ce8947989d403450c92955742a0
cauchumoc1234 <ducanhfo3888@gmail.com>
2020-10-29 16:42:06 +0700

    change in nav bar.

--- js/navbar.js
+++ js/navbar.js
@@ -1,18 +1,3 @@
-$(document).ready(function(){
-    $("#search-btn").click(function(){
-        $("#search-box").fadeToggle("slow")
-    })
-    $(".navbtn").click(function(){
-        
-        $(".main").toggle("slow");
-    })
-    $(window).resize(function(){
-        if(window.outerWidth > 767){
-            $(".main").css("display","flex")
-        }
-        else
-        {
-            $(".main").css("display","none")
-        } 
-    })
+$(".navbtn").click(function(){
+    $(".main").toggleClass("responsive")
 })
